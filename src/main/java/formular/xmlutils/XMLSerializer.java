36ca200d1c8e018c505995d88df1b8fa26c8942f
Matej Sojka <matej.sojka@xit.camp>
2019-10-09 14:31:26 +0200

    dsadasdas

---
+++ src/main/java/formular/xmlutils/XMLSerializer.java
@@ -0,0 +1,20 @@
+package formular.xmlutils;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class XMLSerializer {
+
+	public XMLSerializer() {
+	}
+	
+	public void serializeXML() throws JsonGenerationException, JsonMappingException, IOException{
+	    XmlMapper xmlMapper = new XmlMapper();
+	    xmlMapper.writeValue(new File("simple_bean.xml"), new Object());
+	    File file = new File("simple_bean.xml");
+	}
+}
