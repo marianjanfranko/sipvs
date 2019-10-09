adaf368d0b742a255e4584d9ee1ed448403c978d
Matej Sojka <matej.sojka@xit.camp>
2019-10-09 15:51:01 +0200

    added pojo

---
+++ src/test/java/SimplePojo.java
@@ -0,0 +1,32 @@
+public class SimplePojo {
+	private int x = 5;
+	private int y = 10;
+	private String name = "DASdasda";
+	private double xx = 5.4654;
+	
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getXx() {
+		return xx;
+	}
+	public void setXx(double xx) {
+		this.xx = xx;
+	}
+}
